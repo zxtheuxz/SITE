31dafddb676ca83295d9f5a2703910f5ebeefff2
zxtheuxz <xztheuzx@gmail.com>
2025-03-21 10:10:52 -0300

    PWA

--- vite.config.ts
+++ vite.config.ts
@@ -32,6 +32,22 @@ export default defineConfig({
         start_url: '/',
         display: 'standalone',
         orientation: 'portrait'
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'google-fonts-cache',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365 // 1 ano
+              }
+            }
+          }
+        ]
       }
     })
   ],
